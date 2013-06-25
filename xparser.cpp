5324df2bc521b8166ca5ab3c473adaa061933d0e
Dan Eggleston <eggled@gmail.com>
2013-06-24 20:03:45 -0500

    Made web interface mostly work

--- xparser.cpp
+++ xparser.cpp
@@ -34,13 +34,30 @@ void XParser::add_item(pugi::xml_node node)
 
 void XParser::dump()
 {
-    cout << "<html><head><meta charset=\"utf-8\"></head><body>" << endl;
+    cout << "<html><head><meta charset=\"utf-8\">" << endl;
+    cout << "<style type=\"text/css\">" 
+	<< "#itemwrapper { "
+	<< "border-bottom: 1px solid #CCCCCC; margin-left: 1in; "
+	<< "font-family: arial, sans-serif; font-size: 80%; }"
+	<< "#item { height: 27px; overflow: hidden; white-space: nowrap; "
+	<< " line-height: 27px;  } " 
+	<< "#item a { color: black; font-weight: bold; text-decoration: none } "
+	<< "#item span { display: inline; width: 2in; overflow: hidden; } "
+	<< "</style>" << endl;
+    cout << "<script type=\"text/javascript\">"
+	<< "var shown; function showme(which) {"
+	<< "if (shown) { shown.style.display = 'none'; } shown = document.getElementById(which); "
+	<< "shown.style.display = 'block';  shown.parentNode.scrollIntoView(1); }" 
+	<< "function mj() { if (shown) { showme('spec' + String(parseInt(shown.id.toString().substr(4))+1)) } } "
+	<< "function mk() { if (shown) { showme('spec' + String(parseInt(shown.id.toString().substr(4))-1)) } } "
+	<< " document.onkeypress = function(e) { if (e.charCode == 74 || e.charCode == 106) { mj(); } else if (e.charCode == 75 || e.charCode == 107) { mk(); }  }; "
+	<< "</script>";
+    cout << "</head><body>" << endl;
     cout << this->title << endl;
     cout << this->link << endl;
     cout << this->description << endl;
     for (int i=0; i < this->items.size(); i++)
     {
-        cout << "Item " << i << ":" << endl;
         this->items[i].dump();
     }
     cout << "</body>" << endl;
@@ -56,9 +73,13 @@ subItem::subItem(pugi::xml_node node)
     this->description = node.child_value("description");
     this->content = node.child_value("content:encoded");
 }
-
 void subItem::dump()
 {
-    cout << "<div style=\"overflow: hidden\"><a href=\"" << this->link << "\">" << this->title << "</a>" << this->description << "</div>" << endl;
-    cout << "<div style=\"display: none\">" << this->content << "</div>" << endl;
+	static int specid = 0;
+specid++;
+cout << "<div id=\"itemwrapper\">" << endl;
+    cout << "<div id=\"item\" onclick=\"showme('spec" << specid << "');\">"
+	<< "<a href=\"" << this->link << "\">" << this->title << "</a> - " << this->description << "</div>" << endl;
+    cout << "<div id=\"spec" << specid << "\" style=\"display: none\">" << this->content << "</div>" << endl;
+cout << "</div>" << endl;
 }
