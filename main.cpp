95675dda1013d67482146d60255da8e051f3d93a
Dan Eggleston <eggled@gmail.com>
2013-06-25 21:02:05 -0500

    Use nc -l 8000 < fif | ./rssgen > fif

--- main.cpp
+++ main.cpp
@@ -18,6 +18,14 @@ int main()
         Fetcher g(line);
         display.add(XParser(g.fetch()));
     }
+while (getline(cin, line))
+{
+	if (line.length() == 0) break;
+	if (line.length() == 1 and 13 == line.c_str()[0]) break;
+	cerr << "Got line length " << line.length() << (int) line.c_str()[0] << line << endl;
+}
+cout << "HTTP/1.1 200 OK\r\n";
+    cout << "Content-type: text/html\r\n\r\n";
     display.printpage();
     return 0;
 }
